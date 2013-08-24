25c2265eba8ff577af7f951fb91083adf23dd993
Peter Xin <xinguangyao@gmail.com>
2013-08-24 22:00:09 +0800

    Commit 2013-08-24 22:00:06

--- DataEditor.Adapter/AdvanceImage.cs
+++ DataEditor.Adapter/AdvanceImage.cs
@@ -11,6 +11,7 @@ namespace DataEditor.Adapter
         public FuzzyData.FuzzyString ImageName { get; set; }
         public FuzzyData.FuzzyFixnum ImageIndex { get; set; }
         public FuzzyData.FuzzyFixnum ImageTune { get; set; }
+        /*
         ImageArguments argument;
         public AdvanceImage (XmlNode Node)
         {
@@ -169,6 +170,47 @@ namespace DataEditor.Adapter
                 return sp;
             }
             public ImageSplitIndex GetIndex () { return SplitIndex; }
+        }
+ */
+
+        public enum ImageSplitType { Piece, Size };
+        public class AdvanceImageRect 
+        {
+            public ImageSplitType Type { get; set; }
+            public int XValue { get; set; }
+            public int YValue { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public AdvanceImageRect (ImageSplitType type, int x_value, int y_value, int x, int y)
+            {
+                Type = type;
+                XValue = x_value;
+                YValue = y_value;
+                X = x;
+                Y = y;
+            }
+            public System.Drawing.Rectangle Split (System.Drawing.Rectangle rect, int index = -1)
+            {
+                // 首先，拆分图像并得到数据。
+                int x_width = rect.Width, y_height = rect.Height, x_count, y_count;
+                if ( Type == ImageSplitType.Piece )
+                {
+                    x_count = XValue;
+                    y_count = YValue;
+                    x_width /= x_count;
+                    y_height /= y_count;
+                }
+                else
+                {
+                    x_count = x_width / XValue;
+                    y_count = y_height / YValue;
+                    x_width = XValue;
+                    y_height = YValue;
+                }
+                // 然后，计算图像的位置。
+                if ( index > 0 ) { X = index % x_count; Y = index / x_count; }
+                return new System.Drawing.Rectangle(rect.X + X * x_width, rect.Y + Y * y_height, x_width, y_height);
+            }
         }
     }
 }
---
+++ DataEditor.Control.Wrapper/WrapImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataEditor.Adapter;
+
+namespace DataEditor.Control.Wrapper
+{
+    public class WrapImage
+    {
+
+    }
+    public class ImageArgs : ControlArgs
+    {
+        protected List<AdvanceImage.AdvanceImageRect> Splits = new List<AdvanceImage.AdvanceImageRect>();
+        protected List<AdvanceImage.AdvanceImageRect> Indecis = new List<AdvanceImage.AdvanceImageRect>();
+
+    }
+}
